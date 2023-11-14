ddc3ecebb4fda0c79bd7f3a3e9636a98cc759008
AyeliSuarez <yaramit25@gmail.com>
2023-11-14 11:48:50 -0800

    first commit

--- Web-Scrapping.py
+++ Web-Scrapping.py
@@ -1,3 +1,15 @@
-print(hola)
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-print(bye)
+a = Service(ChromeDriverManager().install())
+opc = Options()
+opc.add_argument("--window-size=700,620")
+navegador = webdriver.Chrome(service=a, options=opc)
+navegador.get("https://www.inegi.org.mx/sistemas/olap/proyectos/bd/continuas/mortalidad/defuncioneshom.asp?s=est&fbclid=IwAR1kKPjPPHvR-lfAfey1c9T2CjT8o0rSfKRmSpgPXYMjH-cyBNMwHEVUOtw")
