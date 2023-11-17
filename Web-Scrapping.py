cf3507d6e7924e5fc14281d90abec683572deec7
AyeliSuarez <yaramit25@gmail.com>
2023-11-17 07:13:48 -0800

    Modificar errores

--- Web-Scrapping.py
+++ Web-Scrapping.py
@@ -26,16 +26,11 @@ entidades = navegador.find_elements(By.CLASS_NAME, "TdInicio")
 for entidad in entidades:
     entidad_text = entidad.text.strip()
 
-    # Si la entidad es una descripción de delito, la ignoramos
     if entidad_text.endswith("/1") or entidad_text.endswith("/2") or entidad_text.endswith("/3"):
         continue
 
     entidad_data = {"Entidad": entidad_text}
-
-    # Encontrar los datos correspondientes a la entidad
     entidad_datos = entidad.find_elements(By.XPATH, "following-sibling::td[@class='Td']")
-
-    # Procesar y agregar los datos al diccionario de la entidad
     for i, tasa_element in enumerate(entidad_datos):
         if i < len(years):
             tasa_incidente_text = tasa_element.text.strip().replace(",", "")
@@ -43,10 +38,8 @@ for entidad in entidades:
 
     data.append(entidad_data)
 
-# Cerrar el navegador
 navegador.quit()
 
-# Crear DataFrame y guardar datos en CSV
 df = pd.DataFrame(data)
 df.to_csv('datasets/incidencia_delictiva_mexico.csv', index=False)
 print("Datos guardados en 'incidencia_delictiva_mexico.csv'")
